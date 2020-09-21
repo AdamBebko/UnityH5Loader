67ee36cc8c1e14a0db352b7cf58a735261424041
adambebko <adambebko@gmail.com>
2020-09-20 23:56:51 -0400

    working h5 dlls

--- H5Loader_Project/Assets/Scripts/H5Load.cs
+++ H5Loader_Project/Assets/Scripts/H5Load.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts {
         void Start() {
             var filePath = Path.Combine(Application.dataPath, "testfile.hdf5");
             long fileId = H5F.open(filePath, H5F.ACC_RDONLY);
-            
+            Debug.Log(fileId);
         }
         
         // Update is called once per frame
